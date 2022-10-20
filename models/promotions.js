785e2f8c9134f9a1fe2038c1f814ed9a53b83746
Ameer Hamza <aha1475@gmail.com>
2022-10-20 14:46:30 +0500

    Added timestamps for leaders and promotions

--- models/promotions.js
+++ models/promotions.js
@@ -3,33 +3,36 @@ require("mongoose-currency").loadType(mongoose);
 const Schema = mongoose.Schema;
 const Currency = mongoose.Types.Currency;
 
-const promotionSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const promotionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    price: {
+      type: Currency,
+      required: true,
+      min: 0,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
   },
-  image: {
-    type: String,
-    required: true,
-  },
-  label: {
-    type: String,
-    default: "",
-  },
-  price: {
-    type: Currency,
-    required: true,
-    min: 0,
-  },
-  featured: {
-    type: Boolean,
-    default: false,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 var promotionModel = mongoose.model("Promotion", promotionSchema);
 
