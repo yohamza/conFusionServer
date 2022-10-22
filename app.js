f13b0c3d2092ce5bc63708ca79dd77ce063f13fe
Ameer Hamza <aha1475@gmail.com>
2022-10-22 20:25:08 +0500

    Authentication with Express Session 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -34,13 +36,21 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth(req, res, next) {
 
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
 
     var authHeader = req.headers.authorization;
 
@@ -58,7 +68,7 @@ function auth(req, res, next) {
     var password = auth[1];
 
     if (username === 'hamza' && password === 'password') {
-      res.cookie('user', 'hamza', { signed: true });
+      req.session.user = 'hamza';
       next();
     }
     else {
@@ -70,7 +80,7 @@ function auth(req, res, next) {
 
   }
   else {
-    if (req.signedCookies.user === 'hamza') {
+    if (req.session.user === 'hamza') {
       next();
     }
     else {
