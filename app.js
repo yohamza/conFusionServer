7ef856dbd69d5fa1c79258176437ab6aaf5f31d8
Ameer Hamza <aha1475@gmail.com>
2022-10-22 19:41:07 +0500

    Basic Auth implemented

--- app.js
+++ app.js
@@ -35,6 +35,40 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    var error = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    error.statusCode = 401;
+    next(error);
+    return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+  var username = auth[0];
+  var password = auth[1];
+
+  if (username == 'admin' && password == 'password') {
+    next();
+  }
+  else {
+    var error = new Error('Wrong username or password');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    error.statusCode = 401;
+    next(error);
+  }
+
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
