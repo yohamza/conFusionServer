57d5c231babb13a21ac1b4c5a22a73f5182b8b69
Ameer Hamza <aha1475@gmail.com>
2022-10-28 20:16:30 +0500

    https with self signed SSL

--- app.js
+++ app.js
@@ -34,6 +34,16 @@ connect.then(
 
 var app = express();
 
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    //Redirect to https if the request came from http
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
